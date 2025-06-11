f0f7517687c44787096bef7a81393e1599d8f2e5
thmastin <thmastin@gmail.com>
2025-06-10 23:34:28 -0400

    Add function to give files in a directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    working_path = os.path.abspath(working_directory)
+    if directory is None:
+        directory_path = working_path
+    else:
+        directory_path = os.path.abspath(directory)
+
+    if directory_path.startswith(working_path) is False:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if os.path.isdir(directory_path) is False:
+        return f'Error: "{directory}" is not a directory'
+    try:
+        files = os.listdir(directory_path)
+        output = []
+        for file in files:
+            file_path = os.path.join(directory_path, file)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            output.append(f"- {file}: file_size={file_size} bytes, is_dir={is_dir}")
+    except Exception as e: 
+            return f"Error: {str(e)}"
+    
+    return "\n".join(output)
