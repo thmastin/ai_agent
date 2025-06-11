b633ad9aedc4a45baa4426c646743810e606afb6
thmastin <thmastin@gmail.com>
2025-06-11 08:47:14 -0400

    Add config

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import sys
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-
+model_name = "gemini-2.0-flash-001"
 client = genai.Client(api_key=api_key)
-
+system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 
 if len(sys.argv) < 2:
     print("A prompt must be provided")
@@ -20,11 +20,11 @@ user_prompt = sys.argv[1]
 
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+response = client.models.generate_content(model=model_name, contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
 print(response.text)
 prompt_usage = response.usage_metadata.prompt_token_count
 response_usage = response.usage_metadata.candidates_token_count
-if sys.argv[2] == "--verbose":
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {prompt_usage}")
     print(f"Response tokens: {response_usage}")
